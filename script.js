6b2cd67f06acb262d6d21b4aeb121e36396b5a64
WeslleiBrito <wesllei.wbs@gmail.com>
2023-01-16 21:48:03 -0300

    Parte 1

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 const frutas = ["laranja", "limão", "uva"];
+
+function incluirItensALista(){
+    let indice = 0
+    for(item of frutas){
+        let fruta = document.getElementById(`fruta-${indice + 1}`)
+        fruta.innerHTML = item
+        indice ++
+    }
+}
+
+
+incluirItensALista()
